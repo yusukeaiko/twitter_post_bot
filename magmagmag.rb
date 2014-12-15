e6989095deb0d610501330beaab3618cce750b13
Yusuke AIKO <yusuke.aiko@gmail.com>
2014-12-15 22:06:30 +0900

    magmagmag bot

---
+++ magmagmag.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+require('twitter')
+require('yaml')
+require('./messages.rb')
+
+tw_config = YAML.load_file(File.expand_path('../twitter.yml', __FILE__))
+twitter = Twitter::REST::Client.new {|config|
+  config.consumer_key        = tw_config['config']['consumer_key']
+  config.consumer_secret     = tw_config['config']['consumer_secret']
+  config.access_token        = tw_config['config']['access_token']
+  config.access_token_secret = tw_config['config']['access_token_secret']
+}
+
+msg   = dekigoto
+index = [*0..msg.count].sample
+twitter.update(msg[index])
